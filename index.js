83c20bb2d75639b25da52facc0d9a261c8cc8b1b
diegogila <dgg26496@hotmail.com>
2023-07-13 20:47:53 -0600

    a

--- index.js
+++ index.js
@@ -10,23 +10,18 @@ app.use(express.static('public'));
 app.get('/weather', async (req, res) => {
   try {
     const { query } = req;
-     
-
     if (!query.location) {
       throw new Error('No se proporcionó ninguna localidad.');
     }
-
     const weatherResponse = await axios.get(
       `http://api.weatherstack.com/current?access_key=${apiKey}&query=${query.location}`
     );
     const weatherData = weatherResponse.data;
-
     res.send(weatherData);
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
 });
-
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });
